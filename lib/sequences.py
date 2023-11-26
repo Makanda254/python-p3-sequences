27911f0f00801ebe9a03b7ccfec4c6ba51dc994a
Victor <victormakanda254@gmail.com>
2023-11-26 20:01:49 +0300

    First commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci_sequence = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_sequence)
+
     pass
